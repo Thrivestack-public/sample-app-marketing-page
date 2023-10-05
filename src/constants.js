e079290fe8278362935da80e78580fc1eab91f2d
Rushikesh Chaudhari <rushikeshc@saastraction.com>
2023-10-05 20:47:32 +0530

    fix: custom signup domain

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,1 @@
-export const thriveSignupUrl =
-  "http://e5ecf992-36b6-11ee-be56-0242ac120002-dev.app.stealthsaasplatform.com/8eb25d05-1520-405a-9a72-81f5a336cab9";
+export const thriveSignupUrl = "https://signup.saasbox.online";
