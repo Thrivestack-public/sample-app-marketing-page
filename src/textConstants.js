1260ea3c3ab59b8f696dd834d2baa4d940e8860f
Rushikesh Chaudhari <rushikeshc@saastraction.com>
2023-10-17 17:09:14 +0530

    chore: updated new text values

--- src/textConstants.js
+++ src/textConstants.js
@@ -1,30 +1,31 @@
 export const textConstants = {
-  HERO_SECTION_WELCOME_CHIP_TEXT: "Welcome to the SaaSBox",
+  HERO_SECTION_WELCOME_CHIP_TEXT:
+    "Welcome to ThriveStack's Sample application - SaaSBox",
   HERO_SECTION_TITLE_PART_ONE: "SaaSBox",
   HERO_SECTION_TITLE_PART_TWO: " - ThriveStack's Sample Application",
   HERO_SECTION_DESCRIPTION:
-    "This is the Marketing Website Page of SaaSBox - a sample application designed to illustrate the seamless integration of ThriveStack's workflows with your own application.",
+    "Imagine this page like your product's marketing page which your end users see. You typically want your end users to either Signup or Login as shown below. You enable this by simply plugging the Signup Workflow, Login Workflow using Thrivestack's Workflow builder.",
 
   PRICING_SECTION_CHIP_TEXT: "Pricing",
   PRICING_SECTION_TITLE: "Pricing Section of SaaSBox",
   PRICING_SECTION_DESCRIPTION:
-    "In the Pricing Section, SaasBuilder can showcase different pricing plans, each with its unique features. Currently, we offer a free pricing plan, and the feature to select among various plans is coming soon. All pricing signups will use the same signup link, but with distinct search parameters, enabling the signup page to identify the specific pricing plan that users wish to subscribe to once this feature is available.",
+    "Imagine the section below as your product's Pricing section or Pricing page that your end users see. Your product can have different pricing plans which can be plugged with disparate Signup Workflows created by you using Thrivestack's Workflow Builder. Notice that when you hover over the Signup buttons under different pricing plans, the URL has different parameters (If you are using Chrome, look at the bottom left of the browser).",
 
   SIGNUP_BUTTON_HOTSPOT_TOOLTIP_NUMBER: "1",
   SIGNUP_BUTTON_HOTSPOT_TOOLTIP_TITLE: "Signup CTA",
   SIGNUP_BUTTON_HOTSPOT_TOOLTIP_TEXT:
-    "This redirects you to SaaSBuilder's SignUp page crafted using the ThriveStack workflow builder.",
+    "Redirects your end users to your product's Signup page configured by you, using our Page builder.",
 
   LOGIN_BUTTON_HOTSPOT_TOOLTIP_NUMBER: "2",
   LOGIN_BUTTON_HOTSPOT_TOOLTIP_TITLE: "Login CTA",
   LOGIN_BUTTON_HOTSPOT_TOOLTIP_TEXT:
-    "This redirects you to SaaSBuilder's Login page crafted using the ThriveStack workflow builder.",
+    "Redirects your end users to your product's Login page configured by you, using our Page builder.",
 
   PRICING_BUTTON_HOTSPOT_TOOLTIP_NUMBER: "3",
   PRICING_BUTTON_HOTSPOT_TOOLTIP_TITLE: "Pricing Specific Signup CTA",
   PRICING_BUTTON_HOTSPOT_TOOLTIP_TEXT:
-    "This redirects you to SaaSBuilder's SignUp page but with pricing specific URL parameters.",
+    "Redirects your end users to your product's SignUp page, but, with pricing-specific URL and parameters.",
 
   SAASBOX_FOOTER_TEXT:
-    "SaaSBox is your scheduling automation platform for eliminating the back-and-forth emails to find the perfect time — and so much more.",
+    "SaaSBox is Thrivestack's sample application to help SaaS Builders like you understand how our platform enables your product to become product-led by integrating Thrivestack's Workflows at various product touchpoints. This sample app also gives a demonstration of the lifecycle of the integrations and your product's end user's journey.",
 };
