a1a63412955b7d6e4f648970b48f914d176e3c75
Rushikesh Chaudhari <rushikeshc@saastraction.com>
2023-10-23 14:20:56 +0530

    fix: section title and image size

--- src/marketing/components/home/ImageSection.js
+++ src/marketing/components/home/ImageSection.js
@@ -7,12 +7,12 @@ function ImageSection() {
     <Fragment>
       <div className="bg-white">
         <div className="relative isolate px-2 lg:px-4">
-          <div className="mx-auto max-w-2xl py-4 sm:py-6 lg:py-8">
-            <div className="text-center">
-              <h1 className="text-2xl font-bold tracking-tight text-violet-900 sm:text-xl ">
+          <div className="mx-auto  py-4 sm:py-6 lg:py-8">
+            <div className="text-center w-full">
+              <h1 className="text-2xl font-bold tracking-tight text-violet-900 sm:text-xl">
                 {textConstants.IMAGE_SECTION_TITLE}
               </h1>
-              <div className="mt-6 w-full">
+              <div className="mt-6 mx-auto max-w-6xl w-full">
                 <img
                   src={`${process.env.PUBLIC_URL}/images/marketing/thrive-image.png`}
                   alt="thrivestack-working"
