cbb7c53021b231ebd23fe503dd1e6eb0025500dc
Ovijeet Sircar <ovijeets@thrivestack.ai>
2024-06-21 15:57:21 +0530

    fix: update runtime url for signup and login

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const thriveSignupUrl = "https://ts.prod.saasproduct.app/f01334c6-f726-11ee-bd2a-e60358d08e04/980b098a-f727-11ee-9452-c6b0e2ef0e53/99ec7e1b-f727-11ee-8fb6-a664c6f522c8/";
-export const thriveLoginUrl = "https://ts.prod.saasproduct.app/f01334c6-f726-11ee-bd2a-e60358d08e04/980b098a-f727-11ee-9452-c6b0e2ef0e53/9d6b7263-f727-11ee-8fb6-a664c6f522c8/";
+export const thriveSignupUrl = "https://ts.app.acme-labs.online/f01334c6-f726-11ee-bd2a-e60358d08e04/980b098a-f727-11ee-9452-c6b0e2ef0e53/99ec7e1b-f727-11ee-8fb6-a664c6f522c8/";
+export const thriveLoginUrl = "https://ts.app.acme-labs.online/f01334c6-f726-11ee-bd2a-e60358d08e04/980b098a-f727-11ee-9452-c6b0e2ef0e53/9d6b7263-f727-11ee-8fb6-a664c6f522c8/";
